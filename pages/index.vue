ef4d3bf4d8240bc64af8bd3a01250391ecf73e46
Aleks <aleksandar.trpkovski@gmail.com>
2024-03-24 10:45:16 +1100

    init

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const htmlString = ref(`
+<h1>Example Heading</h1>
+<p>Example Paragraph</p>
+<a href="/example-1">Example 1</a>
+<a href="/example-2">Example 2</a>
+<a href="/example-3">Example 3</a>`);
+
+onMounted(() => {
+    setTimeout(() => {
+        htmlString.value = `
+            <h1>Example Heading</h1>
+            <p>Example Paragraph</p>
+            <a href="/example-1">Example 1</a>
+            <a href="/example-2">Example 2</a>
+            <a href="/example-3">Example 3</a>
+            <a href="/example-4">Example 4</a>`;
+    }, 4000);
+});
+</script>
+
+<template>
+    <div>
+        <div v-nuxt-html="htmlString"></div>
+    </div>
+</template>
